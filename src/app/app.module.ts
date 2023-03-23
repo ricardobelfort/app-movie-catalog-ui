22c7e49e5ff29db9d178d804865f33ec9d2e824e
Ricardo Belfort <rbelfort2004@gmail.com>
2023-03-23 19:12:49 -0400

    adicionando lib star rating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { MovieListComponent } from './pages/movie-list/movie-list.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+
+import { StarRatingModule } from 'angular-star-rating';
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieListComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, StarRatingModule.forRoot()],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
