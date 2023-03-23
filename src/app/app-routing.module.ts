b2ea767ebcedfe5421dc26d51211c7d514ce65f5
Ricardo Belfort <rbelfort2004@gmail.com>
2023-03-23 18:51:03 -0400

    adicionando rota e ajustes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+import { MovieListComponent } from './pages/movie-list/movie-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MovieListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
